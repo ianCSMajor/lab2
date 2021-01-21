4860961992dc76cf2a1230bdba1073049daa14ea
Koogleblitz <richardtobing@outlook.com>
2021-01-20 16:06:35 -0800

    fix main.cpp to include that other thing

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <string>
+#include "c-echo.h"
+#include "c-count.h"
 
-int main()
+int main( int argv, char** argc)
 {
     std::string name;
     std::cout << "What is your name?" << std::endl;
     std::cin >> name;
     std::cout << "Hello " << name << "!" << std::endl;
+	
+
+//------------new function for counting word---------------------
+
+    std::cout << "Count of Output: " << count(echo(argv,argc)) << std::endl;
 
     return 0;
 }
